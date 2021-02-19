e28ebb231ba8ef14ae791cbcabdf818d9e698525
yuichi oyama <yuichi19991208@gmail.com>
2021-02-19 20:02:33 +0900

    設定関連の処理をconfig.phpに切り出した

---
+++ work/app/config.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+// define('SITE_URL', 'http://localhost8562');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
--- work/public/index.php
+++ work/public/index.php
@@ -1,10 +1,9 @@
 <?php
 
-define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
-define('DB_USER', 'myappuser');
-define('DB_PASS', 'myapppass');
-// define('SITE_URL', 'http://localhost8562');
-define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
+// 相対パスだとエラーになる時があるので、絶対パスで指定してあげる
+require_once(__DIR__ . '/../app/config.php');
+
+createToken();
 
 try {
   $pdo = new PDO(
@@ -27,6 +26,23 @@ function h($str)
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
+function createToken()
+{
+  if (!isset($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+  }
+}
+
+function validateToken()
+{
+  if (
+    empty($_SESSION['token']) ||
+    $_SESSION['token'] !== filter_input(INPUT_POST, 'token')
+  ) {
+    exit('Invalid post request');
+  }
+}
+
 function addTodo($pdo)
 {
   $title = trim(filter_input(INPUT_POST, 'title'));
@@ -47,6 +63,7 @@ function getTodos($pdo)
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  validateToken();
   addTodo($pdo);
 
   header('Location: ' . SITE_URL);
@@ -68,6 +85,7 @@ $todos = getTodos($pdo);
 
   <form action="" method="post">
     <input type="text" name="title" placeholder="Type new todo.">
+    <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
     <!-- <button>Add</button> -->
   </form>
 
