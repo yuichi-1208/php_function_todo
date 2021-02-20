cdd7e52469e7cb2194d6b2c8b36ab334ca373db6
yuichi oyama <yuichi19991208@gmail.com>
2021-02-20 23:58:51 +0900

    クラスを自動で読み込んでくれる機能を実装

--- work/app/config.php
+++ work/app/config.php
@@ -8,7 +8,13 @@ define('DB_PASS', 'myapppass');
 // define('SITE_URL', 'http://localhost8562');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-require_once(__DIR__ . '/Utils.php');
-require_once(__DIR__ . '/Token.php');
-require_once(__DIR__ . '/Database.php');
-require_once(__DIR__ . '/Todo.php');
+spl_autoload_register(function ($class) {
+  $fileName = sprintf(__DIR__ . '/%s.php', $class);
+
+  if (file_exists($fileName)) {
+    require($fileName);
+  } else {
+    echo 'File not found: ' . $fileName;
+    exit;
+  }
+});
